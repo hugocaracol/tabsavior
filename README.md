24b5b9067fb6ecb7fcb16301bac4fca40f2f57fd
hugocaracol <hugonsantos@gmail.com>
2013-08-29 00:57:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Tab savior
+==========
+
+This is a Chrome Extension I created based on a real problem I usually have. When I need to restart my system or the browser, for whatever reason, all the open tabs (with articles to read) are lost.
+
+This is intended to be as simple as possible. You can save tabs or load tabs from a previous save action. It saves tabs from all chrome instances.
+
+Enjoy!
---
+++ chrome_extension/popup.js
@@ -0,0 +1,49 @@
+
+
+var tabSaviour = {
+    
+    loadAction: function(){
+        document.getElementById('message').innerHTML = "Loading tab info...";
+        chrome.storage.local.get('tabs', function (result) {
+            tabs = result.tabs;
+            var tabs_array = tabs.split(",");
+            for(i=0; i<tabs_array.length; i++){
+                var createProperties = {
+                    url: tabs_array[i]
+                };
+                chrome.tabs.create(createProperties);
+            }
+        });
+    },
+    
+    saveAction: function(){
+        parentThis = this;
+        chrome.tabs.query({}, function(tabs) {
+            var tabs_url = new Array();
+            for (i=0; i<tabs.length; i++)
+                tabs_url.push(tabs[i].url);
+            var urls = tabs_url.join();
+            parentThis.saveToLocal(urls);
+
+        });
+    },
+    
+    saveToLocal: function(urls){
+        chrome.storage.local.set({'tabs': urls}, function() {
+            // Notify that we saved.
+            document.getElementById('message').innerHTML = "Tab info saved.";
+        });
+    },
+   
+
+};
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    load_bt = document.getElementById('load_bt');
+    save_bt = document.getElementById('save_bt');
+    load_bt.addEventListener("click", function() { tabSaviour.loadAction();});
+    save_bt.addEventListener("click", function() { tabSaviour.saveAction();});
+
+});
